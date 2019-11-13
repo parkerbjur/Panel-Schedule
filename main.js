f28fc5f213902a94046de6417d5ae40887b47df8
tyof45 <bjur.parker45@gmail.com>
2019-11-13 14:01:55 -0800

    first day

--- main.js
+++ main.js
@@ -1,6 +1,78 @@
 const Exceljs = require('exceljs');
 const fs = require('fs');
 
-const file = fs.readFileSync('./files/Emmaus-27.csv', 'utf8') 
+const file = fs.readFileSync('./files/Emmaus-27.txt', 'utf8')
+const allItemsArray = file.split("\n").map(function(row){return row.split(",");}) 
 
-console.log(file);
+let allRelays = []
+
+let panelA = {
+    size: 48,
+    name: "LCP-A",
+    location: "Elec-102",
+    relays: [
+        {
+            number: "1",
+            ckt: "hd1-13",
+            loadName: "conference 123",
+            emergency: false,
+        },
+        {
+            number: "2",
+            ckt: "hd1-13",
+            loadName: "conference 123",
+            emergency: true,
+        }
+    ]
+}
+
+
+async function copyExcel () {
+    let targetWorkbook = new Ex
+}
+
+
+function printPanel (panel) {
+    const workbook = new Exceljs.Workbook();
+    const worksheet = workbook.addWorksheet(panel.name);
+
+    let sourceWorkbook = new Exceljs.Workbook();
+    sourceWorkbook = sourceWorkbook.xlsx.readFile('./files/source.xlsx');
+    let sourceWorksheet = workbook.getWorksheet('relays')
+
+    let sourceRange;
+    let targetRange;
+
+    switch(panel.size){
+        case 8:
+            sourceRange = ['A2', 'I9']
+            targetRange = ['A1', 'I8']
+        break;
+        case 16:
+            sourceRange = ["A12", 'I23']
+            targetRange = ['A1', 'I12']
+        break;
+        case 32:
+            sourceRange = ['A26', 'I45']
+            targetRange = ['A1', 'I20']
+        break;
+        case 48:
+            sourceRange = ['A48', "I75"]
+            targetRange = ['A1', 'I28']
+        break;
+    }
+    console.log(sourceRange, targetRange)
+    worksheet.mergeCells('A1:I2')
+    worksheet.getCell('A1').value = 'Hello, World!'
+    workbook.xlsx.writeFile('./files/output.xlsx')
+}
+printPanel(panelA)
+
+function sortAllItems(){
+    for (i = 0; i < allItemsArray; i < allItemsArray.length){
+        if(allItemsArray[i][0].includes('Relay')){
+            allRelays.push(allItemsArray[i])
+        }
+    }
+}
+sortAllItems();
